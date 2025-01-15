db3f0326ba18d985aedac46e2f086e1794e3c143
Jota <damatojoel25@gmail.com>
2025-01-15 14:56:42 -0300

    fixed rutas

--- routes/rutas.js
+++ routes/rutas.js
@@ -1,20 +1,12 @@
 // routes/notionRoutes.js
 const express = require('express');
-const router = express.Router();
-const { getCallsDatas } = require('../controllers/ofertasganadas'); // Importa el controlador
-const { getfactt } = require('../controllers/facturacionycash');
-const { getCallsData } = require('../controllers/llamadas');
-const { getInteractionsData } = require('../controllers/estatico'); // Cambié la referencia de "estatico" a "notionController"
+const router = express.Router();// Cambié la referencia de "estatico" a "notionController"
 const webhookController = require('../controllers/webhookController');
 const mongoDataController = require('../controllers/getmongo');
 
 // Ruta para obtener todos los documentos
 router.get('/data', mongoDataController.getAllData);
 router.get('/data/:id', mongoDataController.getDataById);
-router.get('/notion-data', getCallsDatas);
-router.get('/facturacion-cash', getfactt);
-router.get('/llamadas', getCallsData);
-router.get('/estatico', getInteractionsData);
 router.post('/webhook', webhookController.handleWebhook);
 
 module.exports = router;
