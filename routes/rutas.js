77c8145dc0ab0b0b6d95a1a364101cff1fb27b48
Jota <damatojoel25@gmail.com>
2025-01-20 21:24:28 -0300

    fixed test

--- routes/rutas.js
+++ routes/rutas.js
@@ -2,13 +2,13 @@
 const express = require('express');
 const router = express.Router();// Cambié la referencia de "estatico" a "notionController"
 const webhookController = require('../controllers/webhookController');
-const webhookController = require('../controllers/webhookcliente');
+const webhook = require('../controllers/webhookcliente');
 const mongoDataController = require('../controllers/getmongo');
 
 // Ruta para obtener todos los documentos
 router.get('/data', mongoDataController.getAllData);
 router.post('/webhook', webhookController.handleWebhook);
-router.post('/webhook/cliente', webhookController.handleWebhook);
+router.post('/webhook/cliente', webhook.handleWebhook);
 
 
 module.exports = router;
