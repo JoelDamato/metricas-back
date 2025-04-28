f917308fe256243f6b21231aa40a103e78e7d8d9
Joel Damato <damatojoel25@gmail.com>
2025-04-28 17:16:32 -0300

    add

--- models/metricascliente.js
+++ models/metricascliente.js
@@ -3,21 +3,15 @@ const mongoose = require("mongoose");
 const MetricasClienteSchema = new mongoose.Schema(
   {
     id: { type: String, required: true, unique: true },
-
+    Nombre: { type: String }, // Título del cliente
     Agendo: { type: Number }, // Formula - Number
-    "Aplica con CCA": { type: String }, // Formula - String
-
-    "No efectuada con CC": { type: Number }, // (Este no llega, pero lo dejamos)
+    "Aplica Con CC": { type: String }, // Formula - String
     "Call confirm exitoso": { type: Number }, // Formula - Number
-    Origen: { type: String }, // Formula - String
-    Closer: { type: String }, // (Este no llega, pero lo dejamos)
-    Facturacion: { type: Number }, // (Este no llega, pero lo dejamos)
-    "Fecha correspondiente": { type: Date }, // (Este no llega, pero lo dejamos)
     "Fecha de agendamiento": { type: Date }, // Formula - Date
-
-    "Llamadas efectuadas": { type: Number } // Formula - Number
+    "Llamadas efectuadas": { type: Number }, // Formula - Number
+    "Ultimo origen": { type: String } // Formula - String
   },
-  { collection: "metricascliente" } // ✅ Cambié el nombre para que coincida
+  { collection: "metricascliente" }
 );
 
 module.exports = mongoose.model("metricascliente", MetricasClienteSchema);
