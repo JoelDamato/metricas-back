18539386cbb583c0afe50a24a92a7e3d1d53fb3d
Joel Damato <damatojoel25@gmail.com>
2025-04-28 16:48:43 -0300

    add

---
+++ models/metricascliente.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const MetricasDataSchema = new mongoose.Schema(
+  {
+    id: { type: String, required: true, unique: true },
+
+ 
+    Agendo: { type: Number }, // Formula - Number
+    "Aplica con CCA": { type: String }, // Select
+
+    // Campos de llamadas y confirmaciones
+    "No efectuada con CC": { type: Number }, // Formula - Number
+    "Call Confirm No exitoso": { type: Number }, // Formula - Number
+    Origen: { type: String }, // Select
+    "Closer": { type: String },
+    Facturacion: { type: Number }, // Formula - Number
+    "Fecha correspondiente": { type: Date }, // Formula - Date
+    "Fecha de agendamiento": { type: Date },
+    "Llamadas efectuadas": { type: Number }, // Formula - Number
+
+
+  },
+  { collection: "metricas" }
+);
+
+
+MetricasDataSchema.index({ "Venta Meg": 1 });
+MetricasDataSchema.index({ "Llamadas efectuadas": 1 });
+MetricasDataSchema.index({ "Agenda": 1 });
+
+
+
+module.exports = mongoose.model("metricascliente", MetricasDataSchema);
+
+
+
