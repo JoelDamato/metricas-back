574cbefc064715537e63ee9603f09f215a719b5b
Joel Damato <damatojoel25@gmail.com>
2025-04-28 21:44:42 -0300

    add

--- controllers/getclientes.js
+++ controllers/getclientes.js
@@ -1,8 +1,8 @@
-const MetricasCliente = require('../models/metricascliente.js'); // Asegurate que la ruta sea correcta
+const MetricasCliente = require('../models/metricascliente.js');
 
 exports.getMetricasCliente = async (req, res) => {
   try {
-    const metricas = await MetricasCliente.find(); // Podés agregar filtros si querés
+    const metricas = await MetricasCliente.find().lean(); // 🚀 CAMBIO ACÁ
     res.status(200).json(metricas);
   } catch (error) {
     console.error('❌ Error al obtener las métricas:', error);
